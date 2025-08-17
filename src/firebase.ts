6674afdcb1368cd21b55893736b63901bb42ddeb
Khalil <sayahkhalil2001@gmail.com>
2025-08-17 17:39:56 +0200

    firebase setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
+
 const firebaseConfig = {
-  // Replace with your Firebase configuration
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+  apiKey: "AIzaSyA_MlcNaupmISJ_8lEJHYSCLzCr5jmoDXA",
+  authDomain: "cooldown-terminal.firebaseapp.com",
+  databaseURL: "https://cooldown-terminal-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "cooldown-terminal",
+  storageBucket: "cooldown-terminal.firebasestorage.app",
+  messagingSenderId: "753177940178",
+  appId: "1:753177940178:web:e2dcc2892c7efc77b9dd0b",
+  measurementId: "G-WZN4CN14GP"
+}
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
